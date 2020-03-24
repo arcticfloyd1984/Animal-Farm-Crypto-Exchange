cca4b9deb5384ebb370d08f7fcc3a5c9dd806787
Yashasvi <yashasvichaudhry98@gmail.com>
2020-03-24 14:47:48 +0530

    Updated Migration script for a minor functionality

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -12,4 +12,9 @@ module.exports = async function(deployer) {
 
   await deployer.deploy(SheepToken);
   const sheepToken = await SheepToken.deployed();
+
+
+  await dogToken.transfer('0x3D5ee9aCc939f4EaA7e399bDDCc5b1B43649dE23', '1000000000000000000000000');
+  await pigToken.transfer('0xd8c6BA4f55b080acFe238447A3AA8eB5f090e716', '1000000000000000000000000');
+  await sheepToken.transfer('0xEdA8fba34025948F458AC7128AEC97a767965c91', '1000000000000000000000000');
 };
